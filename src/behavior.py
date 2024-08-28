e0f6e66955c94f359a50eab794712723c1d16d14
SkutM <51056882+SkutM@users.noreply.github.com>
2024-08-28 13:47:12 -0700

    Update behavior.py

--- src/behavior.py
+++ src/behavior.py
@@ -6,17 +6,15 @@
 from nodes import *
 from random import randint
 from math import sqrt
-from helper_functions import check_is_player_in_front, chase_player, detect_sound, move_points_of_interest
-
 
 def check_is_roaming(state):
-    return state.principle.stage == 0
+    return state.is_roaming
 
 def check_is_patrolling(state):
-    return state.principle.stage == 1
+    return state.is_patrolling
 
 def check_is_chasing(state):
-    return state.principle.stage == 2
+    return state.is_chasing
         
 def patrol_area(state):
     """
@@ -26,7 +24,20 @@ def patrol_area(state):
     (if features such as hiding in lockers is viable then
     Patrol will open one random)
     """
-    pass
+    patrol_points = state.patrol_points  # TBD
+    current_point = state.current_patrol_point  # where the principal currently is
+    
+    # moves to the next patrol point - cycles thru
+    next_point = (current_point + 1) % len(patrol_points)
+    state.move_to(patrol_points[next_point])
+    
+    # check lockers and areas randomly
+    if randint(0, 100) < 20:  # 20% chance to open a locker
+        locker = state.get_random_locker()
+        state.search_locker(locker)
+    
+    # update patrol state
+    state.current_patrol_point = next_point
     
 
 def create_behavior_tree():
@@ -35,22 +46,21 @@ def create_behavior_tree():
     # ROAM STAGE
 
     roam_stage = Sequence(name="Principal Roaming Stage")
-    
     # Only run this behavior if the state says Principal is currently roaming
     check_roam_status = Check(check_is_roaming)
-    
+
     # Behaviors for roaming
     prioritize_sight = Selector(name="Prioritize Sight")
-    
+
     # Sight-related behavior
-    see_student = Check(check_is_player_in_front)
-    chase_player = Action(chase_player)
+    see_student = Check(check_is_player_in_front)  # Unified sight check
+    chase_student = Action(chase_player)
     sight_behaviors = Sequence(name="Sight Behaviors")
-    sight_behaviors.child_nodes = [see_student, chase_player]
-    
+    sight_behaviors.child_nodes = [see_student, chase_student]
+
     # Sound-related behavior
     investigate_sounds = Sequence(name="Investigate Sounds")
-    check_for_sound = Check(detect_sound)
+    check_for_sound = Check(detect_sound)  # Unified sound check
     move_towards_sound = Action(investigate_sounds)
     investigate_sounds.child_nodes = [check_for_sound, move_towards_sound]
 
@@ -58,15 +68,12 @@ def create_behavior_tree():
     wander_to_points = Sequence(name="Wander to Points of Interest")
     select_point = Action(move_points_of_interest)
     wander_to_points.child_nodes = [select_point]
-    
+
     # Prioritize sight over sound
     prioritize_sight.child_nodes = [sight_behaviors, investigate_sounds]
-    
+
     # Combine all roam behaviors
     roam_stage.child_nodes = [check_roam_status, prioritize_sight, wander_to_points]
-    
-    # Add roam_stage to the root behavior tree
-    root.child_nodes = [roam_stage]
 
     # PATROL STAGE
 
@@ -74,19 +81,35 @@ def create_behavior_tree():
     # only run this behavior if the state says Principle is currently patrolling
     check_patrol_status = Check(check_is_patrolling)
     # TODO: Add patrol behaviors to this branch
-    patrol_stage.child_nodes = [check_patrol_status]
+    patrol_action = Action(patrol_area)
+    patrol_selector = Selector(name="Patrol: Sight and Sound") # prioritize sight over sound
+    patrol_selector.child_nodes = [see_student, check_for_sound, patrol_action] # priority
+    patrol_stage.child_nodes = [check_patrol_status, patrol_selector]  
 
     # CHASE STAGE
 
-    # CHASE STAGE
-    chase_stage = Sequence(name="Principal Chase Stage")
+    chase_stage = Sequence(name="Principle Chase Stage")
+    # only run this behavior if the state says Principle is currently chasing
     check_chase_status = Check(check_is_chasing)
-    
-    # Chase behavior
-    chase_player = Action(chase_player)
-    chase_stage.child_nodes = [check_chase_status, chase_player]
+    # TODO: Add chase behaviors to this branch
 
-    # Add stages to the root behavior tree
+    # 'chase_player' to be implemented
+    chase_player_action = Action(chase_player)
+
+    # if player goes out of sight, move to last known pos
+    move_to_last_seen = ACtion(last_known_position)
+
+    # check if player out of sight for >5 seconds
+    check_out_of_sight_action = Check(check_out_of_sight)
+
+    # chasing -> roaming after player is no longer seen (5+ seconds)
+    stop_chasing = Action(revert_to_roam)
+
+    # Sequence
+    chase_sequence = Sequence(name="Chasing Player")
+    chase_sequence.child_nodes = [chase_player_action, move_to_last_seen, check_out_of_sight_action, stop_chasing]
+
+    # NOTE: This shouldn't need to be modified
     root.child_nodes = [roam_stage, patrol_stage, chase_stage]
 
     return root
