4a018586c5e5097d05f60ac4a4cc558daf2e3063
leonardogs165 <leogsalinas0@gmail.com>
2024-08-27 19:14:27 -0700

    Updating behavior by adding roam and chase

--- src/behavior.py
+++ src/behavior.py
@@ -6,6 +6,8 @@
 from nodes import *
 from random import randint
 from math import sqrt
+from helper_functions import check_is_player_in_front, chase_player, detect_sound, move_points_of_interest
+
 
 def check_is_roaming(state):
     return state.is_roaming
@@ -32,11 +34,39 @@ def create_behavior_tree():
 
     # ROAM STAGE
 
-    roam_stage = Sequence(name="Principle Roaming Stage")
-    # only run this behavior if the state says Principle is currently roaming
+    roam_stage = Sequence(name="Principal Roaming Stage")
+    
+    # Only run this behavior if the state says Principal is currently roaming
     check_roam_status = Check(check_is_roaming)
-    # TODO: Add roam behaviors to this branch
-    roam_stage.child_nodes = [check_roam_status]
+    
+    # Behaviors for roaming
+    prioritize_sight = Selector(name="Prioritize Sight")
+    
+    # Sight-related behavior
+    see_student = Check(check_is_player_in_front)
+    chase_player = Action(chase_player)
+    sight_behaviors = Sequence(name="Sight Behaviors")
+    sight_behaviors.child_nodes = [see_student, chase_player]
+    
+    # Sound-related behavior
+    investigate_sounds = Sequence(name="Investigate Sounds")
+    check_for_sound = Check(detect_sound)
+    move_towards_sound = Action(investigate_sounds)
+    investigate_sounds.child_nodes = [check_for_sound, move_towards_sound]
+
+    # Wander to Points of Interest
+    wander_to_points = Sequence(name="Wander to Points of Interest")
+    select_point = Action(move_points_of_interest)
+    wander_to_points.child_nodes = [select_point]
+    
+    # Prioritize sight over sound
+    prioritize_sight.child_nodes = [sight_behaviors, investigate_sounds]
+    
+    # Combine all roam behaviors
+    roam_stage.child_nodes = [check_roam_status, prioritize_sight, wander_to_points]
+    
+    # Add roam_stage to the root behavior tree
+    root.child_nodes = [roam_stage]
 
     # PATROL STAGE
 
@@ -48,13 +78,15 @@ def create_behavior_tree():
 
     # CHASE STAGE
 
-    chase_stage = Sequence(name="Principle Chase Stage")
-    # only run this behavior if the state says Principle is currently chasing
+    # CHASE STAGE
+    chase_stage = Sequence(name="Principal Chase Stage")
     check_chase_status = Check(check_is_chasing)
-    # TODO: Add chase behaviors to this branch
-    chase_stage.child_nodes = [check_chase_status]
+    
+    # Chase behavior
+    chase_player = Action(chase_player)
+    chase_stage.child_nodes = [check_chase_status, chase_player]
 
-    # NOTE: This shouldn't need to be modified
+    # Add stages to the root behavior tree
     root.child_nodes = [roam_stage, patrol_stage, chase_stage]
 
     return root
